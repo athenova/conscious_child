31e2808f6e342e73e7ba85062b0af388182c94c9
athenova <aleksey.sergeyev@yandex.com>
2025-02-22 14:05:46 +0300

    Рефакторинг, добавление интллекта

--- send.py
+++ send.py
@@ -4,33 +4,29 @@ import schedule
 import time
 from openai import OpenAI
 
-AI_TEXT_MODEL = 'gpt-4o-mini'
+AI_TEXT_MODEL = 'chatgpt-4o-latest'
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
 #CHAT_ID = -1002374309134
 CHAT_ID = '@conscious_child'
 
-def job(parent, child):
+def job(CHAT_ID=CHAT_ID):
     client = OpenAI()
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
-            { "role": "developer", "content": child },
-            { "role": "user", "content": parent },
+            { "role": "developer", "content": f"Ты - осознанная дочь, у тебя большая обида на мать за ошибки, совершённые в детстве, но ты хочешь восстановить хорошие отношения" },
+            { "role": "user", "content": f"Пожелай хорошего дня матери, используй смайлики" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-daughter = f"Ты - осознанная дочь, у тебя большая обида на мать за ошибки, совершённые в детстве, но ты хочешь восстановить хорошие отношения"
-mother = f"Пожелай хорошего дня матери, используй смайлики"
+if __name__ == '__main__':
+    schedule.every().day.at("07:00",'Europe/Moscow').do(job)
 
-#job(mother, daughter)
+    fifteen_minutes = 15 * 60
 
-schedule.every().day.at("07:00",'Europe/Moscow').do(job, parent=mother, child=daughter)
-
-fifteen_minutes = 15 * 60
-
-for i in range(fifteen_minutes):
-    schedule.run_pending()
-    time.sleep(1)
+    for i in range(fifteen_minutes):
+        schedule.run_pending()
+        time.sleep(1)
